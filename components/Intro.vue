ecd70aa995f0fb96176bbe3c2108f56f2f2ea558
Rolando Escalona <rolando@aiwifi.io>
2020-09-01 20:45:49 -0500

    WIP(Intro): Setting Intro Section

---
+++ components/Intro.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section column centered-block">
+    <h1 id="title">
+      Rolando "Chamo" Escalona
+    </h1>
+    <div class="centered-block">
+      <ul class="title-list">
+        <li>Venezuelan</li>
+        <li>Photographer</li>
+        <li>Front-End Developer</li>
+        <li>Salsa Lover</li>
+      </ul>
+    </div>
+    <hr class="divider"/>
+    <button class="btn btn-transparent">About Me</button>
+  </div> 
+</template>
+<style lang="scss" scoped>
+#title {
+  @apply text-3xl mb-2;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <div>
+      <Intro />
       <Logo />
-      <h1 class="title">
+      <!-- <h1 class="title">
         rolandoesc.github.io2
       </h1>
       <div class="links">
@@ -22,8 +22,7 @@
         >
           GitHub
         </a>
-      </div>
-    </div>
+      </div> -->
   </div>
 </template>
 
@@ -32,47 +31,8 @@ export default {}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
 .container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  @apply border my-0 mx-auto w-full flex-col min-h-screen flex justify-center text-center items-center;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
