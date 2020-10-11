d62176e41ae97ab4cfabd013e944429e606141ac
Rolando Escalona <rolando.escalona@gmail.com>
2020-10-11 17:18:38 -0500

    WIP(push -dir): adding push dir command

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  router: {
+    base: '/rolandoesc.github.io/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
