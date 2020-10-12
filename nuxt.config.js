8cdb2c39ae6ab2b205125a675654aefffeb04fce
Rolando Escalona <rolando.escalona@gmail.com>
2020-10-11 22:43:53 -0500

    Fix(Routing): Changing router base to empty

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,25 +11,21 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
-  router: {
-    base: '/rolandoesc.github.io/'
-  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Chamo Escalona',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: `Welcome to Rolando "Chamo" Escalona's Portfolio, Front End Developer` }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
-      // { src: "https://use.fontawesome.com/6d37adc781.js" } 
     ]
   },
   /*
