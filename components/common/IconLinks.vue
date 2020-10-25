f8d3260c38988e296632619cef1c1f66c4e8d250
Rolando Escalona <rolando.escalona@gmail.com>
2020-10-24 20:55:22 -0600

    WIP(Footer): Adding Footer and Telegram Contact

---
+++ components/common/IconLinks.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="icon-links">
+    <a
+      :href="icon['link']"
+      v-for="icon in iconDetails"
+      :key="icon['name']"
+      target="_blank"
+      class="icon-index"
+    >
+      <img :src="icon['icon']" alt class="w-6" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      iconDetails: [
+        {
+          name: "Telegram",
+          icon: require("~/assets/telegram-brands.svg"),
+          link: "https://t.me/rolandoesc",
+        },
+        {
+          name: "Twitter",
+          icon: require("~/assets/twitter-brands.svg"),
+          link: "https://twitter.com/rolandoesc",
+        },
+        {
+          name: "GitHub",
+          icon: require("~/assets/github-brands.svg"),
+          link: "https://github.com/rolandoesc",
+        },
+        {
+          name: "LinkedIn",
+          icon: require("~/assets/linkedin-brands.svg"),
+          link: "https://linkedin.com/in/rolandoesc/",
+        },
+      ],
+    };
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@media screen and (max-width: 679px) {
+  .icon-links {
+    @apply w-4/5 flex justify-start text-left;
+    a.icon-index {
+      @apply mx-4;
+    }
+  }
+}
+.icon-links {
+  @apply flex justify-center;
+  a.icon-index {
+    @apply mx-4;
+  }
+}
+</style>
---
+++ components/sections/footer/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer">
+    <p>
+      2020. All Rights Reserved
+    </p>
+    <IconLinks/>
+  </footer>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  scroll-behavior: smooth;
+  @apply border-t-4 h-12 flex w-4/5 mx-auto border-black justify-between py-4 mb-4;
+}
+
+@media screen and (max-width: 679px) {
+  .footer {
+    @apply flex-col text-left;
+    width: 90%;
+  }
+  .icon-links {
+    @apply justify-start -ml-4 my-4 pb-2;
+  }
+}
+</style>
