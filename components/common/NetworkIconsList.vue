f1f546da4af5d7219f2d25f2975a761209235e9d
Chamo Escalona <rolando.escalona@gmail.com>
2023-12-29 13:28:40 -0600

    Updating work exp

--- components/common/NetworkIconsList.vue
+++ components/common/NetworkIconsList.vue
@@ -1,10 +1,6 @@
 <template>
   <ul class="network-icons-list">
-    <network-icon
-      :icon="icon"
-      v-for="icon in iconDetails"
-      :key="icon['name']"
-    />
+    <network-icon :icon="icon" v-for="icon in iconDetails" :key="icon['name']" v-show="icon['show']" />
   </ul>
 </template>
 
